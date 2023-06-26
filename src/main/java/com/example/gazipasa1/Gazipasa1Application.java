647c9a7c4e60d2a57c580c413ca7b15c41d4715a
denispashin <dpashin@gmail.com>
2023-06-26 07:57:44 +0400

    Initial commit

---
+++ src/main/java/com/example/gazipasa1/Gazipasa1Application.java
@@ -0,0 +1,13 @@
+package com.example.gazipasa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gazipasa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gazipasa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gazipasa1/Gazipasa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gazipasa1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gazipasa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
